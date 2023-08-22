d6bdf04862eabc89e25b3298eb55df8305f4a13d
PimehT <tareerepimeh@gmail.com>
2023-08-22 14:46:09 -0700

    printf

--- functions1.c
+++ functions1.c
@@ -2,7 +2,7 @@
 
 /************************* PRINT UNSIGNED NUMBER *************************/
 /**
- * print_unsigned - Prints an unsigned number
+ * print_unsigned - Prints an unsigned number.
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
@@ -44,7 +44,7 @@ int print_unsigned(va_list types, char buffer[],
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed
+ * Return: Number of chars printed.
  */
 int print_octal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
@@ -86,7 +86,7 @@ int print_octal(va_list types, char buffer[],
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed
+ * Return: Number of chars printed.
  */
 int print_hexadecimal(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
--- functions2.c
+++ functions2.c
@@ -2,7 +2,7 @@
 
 /****************** PRINT POINTER ******************/
 /**
- * print_pointer - Prints the value of a pointer variable
+ * print_pointer - Prints the value of a pointer variable.
  * @types: List a of arguments
  * @buffer: Buffer array to handle print
  * @flags:  Calculates active flags
@@ -61,7 +61,7 @@ int print_pointer(va_list types, char buffer[],
  * @width: get width
  * @precision: Precision specification
  * @size: Size specifier
- * Return: Number of chars printed
+ * Return: Number of chars printed.
  */
 int print_non_printable(va_list types, char buffer[],
 	int flags, int width, int precision, int size)
